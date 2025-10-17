a5f2c13024975e30c41848adf5683e00c358a02c
lijian <1286958193@qq.com>
2025-10-17 20:20:29 +0800

    commit inition

---
+++ Hospital-Appointment-Registration-Systems-common/src/main/java/com/graduation/common/ErrorCode.java
@@ -0,0 +1,31 @@
+package com.graduation.common;
+
+/**
+ * 自定义错误码
+ */
+public enum ErrorCode {
+    SUCCESS(0, "ok"),
+    PARAMS_ERROR(40000, "请求参数错误"),
+    NULL_ERROR(40001, "请求数据为空"),
+    NOT_LOGIN(40100, "未登录"),
+    NO_AUTH(40101, "无权限"),
+    SYSTEM_ERROR(50000, "系统内部异常");
+    // 状态码
+    private final int code;
+
+    // 信息
+    private final String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
